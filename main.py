145a358fa736ea0d9b922c82ffb919d1ff309ab6
Evelin Kolev <evelinkolev@vivaldi.net>
2024-04-22 12:16:29 +0200

    Use grid sticky directions to make the UI for Windows look nicer

--- main.py
+++ main.py
@@ -17,25 +17,25 @@ website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
 website_entry = Entry(bg="white", width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
 
 email_username_label = Label(text="Email/Username:")
 email_username_label.grid(column=0, row=2)
 
 email_username_entry = Entry(bg="white", width=35)
-email_username_entry.grid(column=1, row=2, columnspan=2)
+email_username_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
-password_entry = Entry(bg="white", width=21)
-password_entry.grid(column=1, row=3)
+password_entry = Entry(bg="white")
+password_entry.grid(column=1, row=3, sticky="W")
 
 generate_password_button = Button(text="Generate Password")
-generate_password_button.grid(column=2, row=3)
+generate_password_button.grid(column=2, row=3, sticky="E")
 
 add_button = Button(text="Add", width=30)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 
 window.mainloop()
